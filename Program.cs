bd798894935d92f98f2a49d04b7d83f4887ae635
MatheusKhairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-04-24 20:28:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um insulto para uma pessoa: ");
+            string insulto=Console.ReadLine().Replace("chato", "#*$#*").Replace("feio", "@**%");
+
+            Console.Clear();
+            
+            string mensagem="" +insulto+ "";
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
